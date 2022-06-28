2c832a34b5288e224e106457ab2489d08f9fc36f
bluelotus03 <25125692+bluelotus03@users.noreply.github.com>
2022-06-28 13:59:56 -0400

    scenario with specific data complete

--- src/main/java/stepDefinitions/Contact_Us_Steps.java
+++ src/main/java/stepDefinitions/Contact_Us_Steps.java
@@ -47,6 +47,8 @@ public class Contact_Us_Steps {
         driver.get("https://webdriveruniversity.com/Contact-Us/contactus.html");
         System.out.println("✅ Accessed the website");
     }
+
+    /** Unique Data Step Definitions **/
     @When("I enter a unique first name")
     public void i_enter_a_unique_first_name() {
         driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("AutoFN" + generateRandomNumber(5));
@@ -67,27 +69,30 @@ public class Contact_Us_Steps {
         driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys("Hello! " + generateRandomString(20));
         System.out.println("✅ Entered generated message");
     }
+    /** ------------------------------- **/
 
+    /** Specific Data Step Definitions **/
     @When("I enter a specific first name {word}")
     public void i_enter_a_specific_first_name(String firstName) {
-
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstName);
         System.out.println("✅ Added first name: " + firstName);
     }
     @When("I enter a specific last name {word}")
     public void i_enter_a_specific_last_name(String lastName) {
-
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys(lastName);
         System.out.println("✅ Added last name: " + lastName);
     }
     @When("I enter a specific email address {word}")
     public void i_enter_a_specific_email_address(String emailAddress) {
-
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(emailAddress);
         System.out.println("✅ Added email address: " + emailAddress);
     }
     @When("I enter a specific comment {string}")
     public void i_enter_a_specific_comment(String comment) {
-
+        driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys(comment);
         System.out.println("✅ Added comment: " + comment);
     }
+    /** ------------------------------- **/
 
     @And("I click the submit button")
     public void i_click_the_submit_button() {
