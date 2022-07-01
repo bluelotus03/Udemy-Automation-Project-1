e2339f7375b90c296bc93dfb4590fedd79b377e5
bluelotus03 <25125692+bluelotus03@users.noreply.github.com>
2022-07-01 11:41:53 -0400

    started Login_PO

--- src/main/java/stepDefinitions/Login_Steps.java
+++ src/main/java/stepDefinitions/Login_Steps.java
@@ -7,30 +7,38 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.*;
 import org.testng.Assert;
 import pageObject.Base_PO;
+import pageObject.Login_PO;
 
 public class Login_Steps extends Base_PO {
     private WebDriver driver = getDriver();
 
+    private Login_PO login_po;
+    public Login_Steps(Login_PO login_po) {
+        this.login_po = login_po;
+    }
     @Given("I access the WebDriver University login page")
     public void i_access_the_web_driver_university_login_page() {
-        navigateTo_URL("https://webdriveruniversity.com/Login-Portal/index.html");
+        login_po.navigateTo_WebDriverUniversity_Login_Page();
         System.out.println("✅ Accessed the webpage");
     }
 
     @When("I enter a username {word}")
     public void i_enter_a_username(String username) {
-        driver.findElement(By.id("text")).sendKeys(username);
+        //driver.findElement(By.id("text")).sendKeys(username);
+        login_po.setUsername(username);
         System.out.println("✅ Added username: " + username);
     }
     @And("I enter a password {word}")
     public void i_enter_a_password(String password) {
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+        //driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+        login_po.setPassword(password);
         System.out.println("✅ Added password: " + password);
     }
 
     @And("I click the login button")
     public void i_click_the_login_button() {
-        driver.findElement(By.xpath("//button[@id='login-button']")).click();
+        //driver.findElement(By.xpath("//button[@id='login-button']")).click();
+        login_po.clickOnLoginButton();
         System.out.println("✅ Pressed login button");
     }
 
